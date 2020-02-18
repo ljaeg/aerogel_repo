e5ad26f583713f42beda91d16627659e6b15cc13
Logan Jaeger <loganjaeger@berkeley.edu>
2020-02-18 12:39:33 -0800

    get some images

---
+++ getSomeImages.py
@@ -0,0 +1,61 @@
+import os
+import numpy as np 
+from PIL import Image
+import h5py
+import random
+
+Dir = "/home/admin/Desktop/aerogel_preprocess"
+datafile_name = "first_iteration.hdf5"
+SaveDir = "/home/admin/Desktop/aerogel_repo/fromHDF"
+
+DF = h5py.File(os.path.join(save_dir, datafile_name), "r")
+TrainYes = DF["TrainYes"]
+TrainNo = DF["TrainNo"]
+
+for_seed = 139
+np.random.seed(for_seed)
+
+numIms1 = TrainYes.shape[0]
+numIms2 = TrainNo.shape[0]
+
+yes_inds = np.random.random_integers(0, high = numIms1, size = 3)
+no_inds = np.random.random_integers(0, high = numIms2, size = 3)
+
+for j in [0, 1, 2]:
+	direc = os.path.join(SaveDir, "yes" + str(j))
+	i = 0
+	while True:
+		try:
+			im_slice = TrainYes[yes_inds[j], i, :, :, :]
+			im = Image.from_array(im_slice)
+			im.save(os.path.join(SaveDir, "yes" + str(j), str(i) + ".png"))
+			i += 1
+		except IndexError:
+			f = open(os.path.join(SaveDir, "yes" + str(j), "info.txt"), "w")
+			f.write("j=" + str(j) + "\n")
+			f.write("yes index: " + str(yes_inds[j]) + "\n")
+			f.write("seed: " + str(for_seed) + "\n")
+			f.write("Dir: " + Dir + "\n")
+			f.write("datafile_name: " + datafile_name + "\n")
+			f.write("SaveDir: " + SaveDir + "\n")
+			f.close()
+			break
+
+for j in [0, 1, 2]:
+	i = 0
+	while True:
+		try:
+			im_slice = TrainYes[yes_inds[j], i, :, :, :]
+			im = Image.from_array(im_slice)
+			im.save(os.path.join(SaveDir, "no" + str(j), str(i) + ".png"))
+			i += 1
+		except IndexError:
+			f = open(os.path.join(SaveDir, "no" + str(j), "info.txt"), "w")
+			f.write("j=" + str(j) + "\n")
+			f.write("no index: " + str(no_inds[j]) + "\n")
+			f.write("seed: " + str(for_seed) + "\n")
+			f.write("Dir: " + Dir + "\n")
+			f.write("datafile_name: " + datafile_name + "\n")
+			f.write("SaveDir: " + SaveDir + "\n")
+			f.close()
+			break
