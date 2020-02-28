06319587c1ec344b4a7ddb4273f5ab0b4db78b46
Logan Jaeger <loganjaeger@berkeley.edu>
2020-02-28 09:14:12 -0800

    k

--- construct.py
+++ construct.py
@@ -83,6 +83,8 @@ def insert_blank_mask(mask_id, to_dir, shape = None):
 	key_blank = random.choice(list(testSurfaceFinders.d.keys()))
 	blank_surface = testSurfaceFinders.d[key_blank]
 	blank, __ = load_and_getDelSq(Dir + "forTestingSurface/" + key_blank)
+	if blank.shape == (0,):
+		print("blank is from: {}".format(key_blank))
 	small_blank = get_subimages(blank, mask.shape)
 	small_blank, mask = augment(small_blank, mask)
 	background, background_sur = load_and_getDelSq(to_dir)
