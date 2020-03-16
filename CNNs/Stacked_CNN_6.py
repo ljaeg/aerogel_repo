84d37ad79cfdf12a1ee0d8658401a54321a3b1e1
Logan Jaeger <loganjaeger@berkeley.edu>
2020-03-16 10:52:54 -0700

    ddd

--- CNNs/Stacked_CNN_6.py
+++ CNNs/Stacked_CNN_6.py
@@ -179,7 +179,7 @@ model.compile(optimizer=Nadam(lr=0.00015), loss='binary_crossentropy', metrics=[
 Checkpoint_Loss = ModelCheckpoint('/home/admin/Desktop/aerogel_CNNs/loss_FOV100.h5', verbose=1, save_best_only=True, monitor='val_loss')
 Checkpoint_Acc = ModelCheckpoint('/home/admin/Desktop/Saved_CNNs/acc_FOV100.h5', verbose=1, save_best_only=True, monitor='val_acc')
 model.fit(
-	generator = TrainGenerator,
+	x = TrainGenerator,
 	steps_per_epoch = len(trainAnswers) // batch_size,
 	epochs = 500,
 	verbose = 2,
