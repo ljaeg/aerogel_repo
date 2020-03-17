e6b6bd6489c97a0bea560d3c3c6a30e215d2a339
Logan Jaeger <loganjaeger@berkeley.edu>
2020-03-17 12:48:02 -0700

    d

--- CNNs/Stacked_CNN_6.py
+++ CNNs/Stacked_CNN_6.py
@@ -41,7 +41,7 @@ datafile_path = os.path.join(Dir, h5_file)
 batch_size = 32
 class_weights = {0:1, 1:1} #Just in case you want to make the NN biased towards positives or negatives
 conv_scale = 32
-dense_scale = 128 // 4
+dense_scale = 128
 dropout_rate = .25
 spatial_d_rate = .25
 
