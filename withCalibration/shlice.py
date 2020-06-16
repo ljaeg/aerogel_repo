6576b3786c2737fe3992ef14a9c1628840a7a222
Logan Jaeger <loganjaeger@berkeley.edu>
2020-06-16 15:01:22 -0700

    ready to do shlice

--- withCalibration/shlice.py
+++ withCalibration/shlice.py
@@ -2,18 +2,20 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import pandas as pd
 import os
+import time
 
 image_size = (150, 150) #how big do you want the sliced image to be?
-original_size = (512, 384)
-yx = (384, 512)
-original_size = yx
+original_size = (384, 512)
 
-code_places = {
-	'fm_-59163_18844' : (24, 249),
-	'fm_-100689_-12884' : (158, 288),
-	'fm_-7366_-82272' :  (133, 351)
-} # key is the amazon code, while the value is a tuple of (x_coord, y_coord) from flair.
+csv = pd.read_csv('containsCoords.csv')
+
+def get_coords(code):
+	sub = csv[csv.amazon_key == code]
+	xc = sub.x_coord.item()
+	yc = sub.y_coord.item()
+	return xc, yc
 
 def determine_slice_placement(tpl):
 	x_coord = tpl[0]
@@ -34,12 +36,18 @@ def determine_coord(coord, n): #if y, make n=0. If x, make n=1.
 		atpl = (original_size[n] - image_size[n], original_size[n])
 	return (int(atpl[0]), int(atpl[1]))
 
+def get_str_from_number(n):
+	if n < 10:
+		return '-00' + str(n)
+	else:
+		return '-0' + str(n)
+
 def load_movie(code):
 	#load in the movie
 	X = []
-	frame = 3
+	frame = 1
 	while True:
-		path = code + "/" + str(frame) + ".png"
+		path = os.path.join("..", "..", "aerogel_preprocess", "calAmazon", code, code + get_str_from_number(frame) + ".jpg")
 		try:
 			img = plt.imread(path)
 			X.append(img)
@@ -50,21 +58,21 @@ def load_movie(code):
 	return X
 
 def slice_movie(code):
-	where_track = code_places[code]
+	where_track = get_coords(code)
 	xs, ys = determine_slice_placement(where_track)
-	print(xs)
-	print(ys)
+	# print(xs)
+	# print(ys)
 	orig_movie = load_movie(code)
-	print(orig_movie.shape)
+	#print(orig_movie.shape)
 	sliced = np.zeros((orig_movie.shape[0], image_size[0], image_size[1], orig_movie.shape[3]))
-	print(sliced.shape)
-	rectangle_movie(code, orig_movie, xs[0], ys[0])
+	#print(sliced.shape)
+	#rectangle_movie(code, orig_movie, xs[0], ys[0])
 	for i in range(orig_movie.shape[0]):
 		sliced[i, :, :, :] = orig_movie[i, ys[0]:ys[1], xs[0]:xs[1], :]
 	return sliced
 
 def save_new_movie(code, sliced_movie):
-	direc = "SLICED-"+code
+	direc = os.path.join("..", "..", 'aerogel_preprocess', 'sliced', code)
 	if not os.path.isdir(direc): 
 		os.mkdir(direc)
 	i = 1
@@ -87,9 +95,28 @@ def rectangle_movie(code, movie, x, y):
 		plt.savefig(f'rectangular/{i}.png')
 		plt.close()
 
-def do(code):
+def do_single(code):
 	sm = slice_movie(code)
 	save_new_movie(code, sm)
 
-do('fm_-7366_-82272')
+def time_left(current, total, time_elapsed):
+	pace = current / time_elapsed
+	n_left = total - current
+	return n_left / pace
+
+def do_all():
+	directory = os.path.join("..", "..", "aerogel_preprocess", "calAmazon")
+	allcodes = [x[0] for x in os.walk(directory)]
+	number_to_do = len(allcodes)
+	current_number = 0
+	t0 = time.time()
+	for code in allcodes:
+		do_single(code)
+		current_number += 1
+		t1 = time.time()
+		t_left = time_left(current_number, number_to_do, (t1 - t0) / 60)
+		print(f' done: {current_number}/{number_to_do}. Time left: {t_left}', flush=True, end = '\r')
+
+
+do_all()
 
